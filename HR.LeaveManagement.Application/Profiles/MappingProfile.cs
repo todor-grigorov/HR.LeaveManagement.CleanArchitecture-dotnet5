ad12f75e4545d3b9e146a4a56a851034120ee857
todor-grigorov <t.grigorov99@gmail.com>
2023-09-16 21:59:38 +0200

    feat: add empty mpping profiel class

---
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+
+    }
+}
