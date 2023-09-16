01c90fed0282c59eb53719e55888c150fc7516db
todor-grigorov <t.grigorov99@gmail.com>
2023-09-16 22:03:51 +0200

    fix: type of LeaveType prop

--- HR.LeaveManagement.Application/DTOs/LeaveRequestDto.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequestDto.cs
@@ -10,7 +10,7 @@ namespace HR.LeaveManagement.Application.DTOs
     {
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public LeaveType LeaveType { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
         public DateTime DateRequested { get; set; }
         public string RequestComments { get; set; }
