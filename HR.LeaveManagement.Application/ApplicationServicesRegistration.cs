b23eb2893940ebd02d234b816ea04447ddfa2338
todor-grigorov <t.grigorov99@gmail.com>
2023-09-17 23:45:03 +0200

    feat: AddMediatR to ConfigureApplicationServices

--- HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -8,9 +8,12 @@ namespace HR.LeaveManagement.Application
 {
     public static class ApplicationServicesRegistration
     {
-        public static void ConfigureApplicationServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            return services;
         }
     }
 }
