57fbb298a4531c1c1ec1abf481d991fef47b9e9a
todor-grigorov <t.grigorov99@gmail.com>
2023-09-19 23:31:33 +0200

    feat: add GetLeaveTypeDetailRequest

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Requests/GetLeaveTypeDetailRequest.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Requests
+{
+    public class GetLeaveTypeDetailRequest : IRequest<LeaveTypeDto>
+    {
+        public int Id { get; set; }
+    }
+}
