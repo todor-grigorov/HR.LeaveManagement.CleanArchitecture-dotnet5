0f14ddd8c278242222ed1e9c8f1a60646e09d6b9
todor-grigorov <t.grigorov99@gmail.com>
2023-09-21 20:42:00 +0200

    feat: add GetLeaveAlocationListRequest

---
+++ HR.LeaveManagement.Application/Features/LeaveAlocations/Requests/Queries/GetLeaveAlocationListRequest.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAlocations.Requests.Queries
+{
+    public class GetLeaveAlocationListRequest : IRequest<List<LeaveAllocationDto>>
+    {
+    }
+}
