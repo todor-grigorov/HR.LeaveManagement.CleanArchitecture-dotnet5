02af7d5977ec300d4c3b5bfbeaaf539accbd5df1
todor-grigorov <t.grigorov99@gmail.com>
2023-09-24 12:25:29 +0200

    feat: add LeaveRequestListDto

---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequestListDto.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs
+{
+    public class LeaveRequestListDto : BaseDto
+    {
+        public string RequestingEmployeeId { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public DateTime DateRequested { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool? Approved { get; set; }
+    }
+}
