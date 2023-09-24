59b7b4854c86e84f3d1f36c72ec52d027ac9fa14
todor-grigorov <t.grigorov99@gmail.com>
2023-09-24 12:12:43 +0200

    feat: add methods in ILeaveRequestRepository

--- HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
+++ HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -1,12 +1,15 @@
-﻿using HR.LeaveManagement.Domain;
+﻿using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Persistence.Contracts
 {
     public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
     {
-
+        Task<LeaveRequest> GetLeaveRequestWithDetails(int id);
+        Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
     }
 }
