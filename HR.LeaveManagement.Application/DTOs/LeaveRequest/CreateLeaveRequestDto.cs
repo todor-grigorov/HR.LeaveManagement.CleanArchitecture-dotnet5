fb8482c23f2f752e27dc40df4a7a6602910b05be
todor-grigorov <t.grigorov99@gmail.com>
2023-09-24 19:52:04 +0200

    fix: remnove DateRequested prop from CreateLeaveRequestDto

--- HR.LeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
@@ -10,7 +10,6 @@ namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public int LeaveTypeId { get; set; }
-        public DateTime DateRequested { get; set; }
         public string RequestComments { get; set; }
     }
 }
