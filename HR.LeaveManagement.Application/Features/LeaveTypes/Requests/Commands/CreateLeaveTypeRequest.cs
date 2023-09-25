0387dbcc06dd78fa8da3b440a0415183818c22d1
todor-grigorov <t.grigorov99@gmail.com>
2023-09-25 17:03:33 +0200

    feat: add CreateLeaveTypeRequest

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/CreateLeaveTypeRequest.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveType;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands
+{
+    public class CreateLeaveTypeRequest : IRequest<int>
+    {
+        public LeaveTypeDto LeaveTypeDto { get; set; }
+    }
+}
