5c709dbcacdcba6ac9a50e61b8b6a95e0d056f91
todor-grigorov <t.grigorov99@gmail.com>
2023-09-27 20:31:04 +0200

    feat: add CreateLeaveAllocationCommand

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Requests/Commands/CreateLeaveAllocationCommand.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands
+{
+    public class CreateLeaveAllocationCommand : IRequest<int>
+    {
+        public LeaveAllocation LeaveAllocationDto { get; set; }
+    }
+}
