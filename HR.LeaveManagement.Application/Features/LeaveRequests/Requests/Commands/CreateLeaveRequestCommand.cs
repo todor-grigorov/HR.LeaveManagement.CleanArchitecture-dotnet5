d7de5c3a733e2316b79a6052d140a0cc4459f98b
todor-grigorov <t.grigorov99@gmail.com>
2023-09-28 21:07:48 +0200

    feat: add CreateLeaveRequestCommand

---
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Requests/Commands/CreateLeaveRequestCommand.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands
+{
+    public class CreateLeaveRequestCommand : IRequest<int>
+    {
+        public LeaveRequestDto LeaveRequestDto { get; set; }
+    }
+}
