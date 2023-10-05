86028b4d7d0e9f334a80bbd6ee43a5cfc15ac5f9
todor-grigorov <t.grigorov99@gmail.com>
2023-10-05 16:15:50 +0200

    feat: add UpdateLeaveRequestCommandHandler

---
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/UpdateLeaveRequestCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class UpdateLeaveRequestCommandHandler : IRequestHandler<UpdateLeaveRequestCommand, Unit>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            this._leaveRequestRepository = leaveRequestRepository;
+            this._mapper = mapper;
+        }
+        public async Task<Unit> Handle(UpdateLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveReuqest = await _leaveRequestRepository.Get(request.LeaveRequestDto.Id);
+
+            _mapper.Map(request.LeaveRequestDto, leaveReuqest);
+
+            await _leaveRequestRepository.Update(leaveReuqest);
+
+            return Unit.Value;
+        }
+    }
+}
