5f5951000f4bc47ea49b07f5fe3a49c1c79f40b1
todor-grigorov <t.grigorov99@gmail.com>
2023-10-06 16:20:19 +0200

    feat: add ChangeLeaveRequestApprovalDto prop to UpdateLeaveRequestCommand

--- HR.LeaveManagement.Application/Features/LeaveRequests/Requests/Commands/UpdateLeaveRequestCommand.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Requests/Commands/UpdateLeaveRequestCommand.cs
@@ -9,5 +9,7 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Command
     public class UpdateLeaveRequestCommand : IRequest<Unit>
     {
         public UpdateLeaveRequestDto LeaveRequestDto { get; set; }
+        public ChangeLeaveRequestApprovalDto ChangeLeaveRequestApprovalDto { get; set; }
+
     }
 }
