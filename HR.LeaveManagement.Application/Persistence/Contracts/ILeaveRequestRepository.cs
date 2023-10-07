2cbe2959f6a7530eb52638e7ba6cb6b9925b355b
todor-grigorov <t.grigorov99@gmail.com>
2023-10-07 16:36:43 +0200

    feat: add ChangeApprovalStatus method to ILeaveRequestRepository

--- HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
+++ HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -11,5 +11,6 @@ namespace HR.LeaveManagement.Application.Persistence.Contracts
     {
         Task<LeaveRequest> GetLeaveRequestWithDetails(int id);
         Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
+        Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? approvalStatus);
     }
 }
