6a47657bd424d45503c1fcbe6be5f7940c79ac3e
todor-grigorov <t.grigorov99@gmail.com>
2023-10-09 20:01:03 +0200

    feat: add DeleteLeaveTypeCommandHandler

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+
+            await _leaveTypeRepository.Delete(leaveType);
+
+            return Unit.Value;
+        }
+    }
+}
