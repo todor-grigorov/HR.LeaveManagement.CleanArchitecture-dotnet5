16021197d6675889ea74f83bcaafb42973b41746
todor-grigorov <t.grigorov99@gmail.com>
2023-10-13 22:09:13 +0200

    feat: add CreateLeaveTypeDtoValidator

---
+++ HR.LeaveManagement.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveType.Validators
+{
+    public class CreateLeaveTypeDtoValidator : AbstractValidator<CreateLeaveTypeDto> 
+    {
+        public CreateLeaveTypeDtoValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50)
+                .WithMessage("{PropertyName} must not exceed 50 characters.");
+
+            RuleFor(p => p.DefaultDays)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+                .LessThan(100).WithMessage("{PropertyName} must be kess than 100.");
+        }
+    }
+}
