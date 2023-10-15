8c74853cd0f05f5a6737dfe3501c0a86cf9feadd
todor-grigorov <t.grigorov99@gmail.com>
2023-10-15 23:51:31 +0200

    feat: add CreateLeaveAllocationDtoValidator

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllocation/Validators/CreateLeaveAllocationDtoValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveAllocation.Validators
+{
+    public class CreateLeaveAllocationDtoValidator : AbstractValidator<CreateLeaveAllocationDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveAllocationDtoValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            RuleFor(p => p.NumebrOfDays)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+                .LessThan(100).WithMessage("{PropertyName} must be kess than 100.");
+
+            RuleFor(p => p.LeaveTypeId)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+                .NotNull()
+                 .MustAsync(async (id, token) =>
+                 {
+                     var leaveTypeExists = await _leaveTypeRepository.Exists(id);
+                     return leaveTypeExists;
+                 })
+                .WithMessage("{PropertyName} does not exist.");
+
+            RuleFor(p => p.Period)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+                .LessThan(100).WithMessage("{PropertyName} must be kess than 100.");
+        }
+    }
+}
