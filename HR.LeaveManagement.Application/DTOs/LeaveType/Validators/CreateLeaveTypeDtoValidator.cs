0475db611942d1397169b436041bff405c966237
todor-grigorov <t.grigorov99@gmail.com>
2023-10-19 22:52:39 +0200

    refactor: CreateLeaveTypeDtoValidator to Include ILeaveTypeDtoValidator

--- HR.LeaveManagement.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
@@ -10,17 +10,7 @@ namespace HR.LeaveManagement.Application.DTOs.LeaveType.Validators
     {
         public CreateLeaveTypeDtoValidator()
         {
-            RuleFor(p => p.Name)
-                .NotEmpty()
-                .WithMessage("{PropertyName} is required.")
-                .NotNull()
-                .MaximumLength(50)
-                .WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.");
-
-            RuleFor(p => p.DefaultDays)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
-                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
-                .LessThan(100).WithMessage("{PropertyName} must be kess than {ComparisonValue}.");
+            Include(new ILeaveTypeDtoValidator());
         }
     }
 }
