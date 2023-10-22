e75807136405711fa0f8447b0c657d1241c9057d
todor-grigorov <t.grigorov99@gmail.com>
2023-10-22 23:08:38 +0200

    refactor: CreateLeaveRequestDtoValidator to Include ILeaveRequestDtoValidator

--- HR.LeaveManagement.Application/DTOs/LeaveRequest/Validators/CreateLeaveRequestDtoValidator.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/Validators/CreateLeaveRequestDtoValidator.cs
@@ -13,21 +13,7 @@ namespace HR.LeaveManagement.Application.DTOs.LeaveRequest.Validators
         public CreateLeaveRequestDtoValidator(ILeaveRequestRepository leaveRequestRepository)
         {
             _leaveRequestRepository = leaveRequestRepository;
-
-            RuleFor(p => p.StartDate)
-                .LessThan(p => p.EndDate).WithMessage("{PropertyName} must be before {ComparisonValue}");
-
-            RuleFor(p => p.EndDate)
-                .GreaterThan(p => p.StartDate).WithMessage("{PropertyName} must be after {ComparisonValue}");
-
-            RuleFor(p => p.LeaveTypeId)
-                .GreaterThan(0)
-                .MustAsync(async (id, token) =>
-                {
-                    var leaveTypeExists = await _leaveRequestRepository.Exists(id);
-                    return !leaveTypeExists;
-                })
-                .WithMessage("{PropertyName}  does not exist.");
+            Include(new ILeaveRequestDtoValidator(_leaveRequestRepository));
         }
     }
 }
