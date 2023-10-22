b8ac2006b756664461fc811cfcaaf83b38304a4d
todor-grigorov <t.grigorov99@gmail.com>
2023-10-23 00:38:39 +0200

    feat: add ILeaveAllocationDto

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllocation/ILeaveAllocationDto.cs
@@ -0,0 +1,14 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveType;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveAllocation
+{
+    public interface ILeaveAllocationDto
+    {
+        public int NumberOfDays { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
