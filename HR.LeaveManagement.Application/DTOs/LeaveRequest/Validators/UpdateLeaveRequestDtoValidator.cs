a245766bfeaebbfc04b503af54cfe490df9b1bb8
todor-grigorov <t.grigorov99@gmail.com>
2023-10-23 23:11:38 +0200

    feat: add UpdateLeaveRequestDtoValidator

---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/Validators/UpdateLeaveRequestDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequest.Validators
+{
+    public class UpdateLeaveRequestDtoValidator : AbstractValidator<UpdateLeaveRequestDto>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+        public UpdateLeaveRequestDtoValidator(ILeaveRequestRepository leaveRequestRepository)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            Include(new ILeaveRequestDtoValidator(_leaveRequestRepository));
+
+            RuleFor(p => p.Id).NotEmpty().WithMessage("{PropertyName} is required");
+        }
+    }
+}
